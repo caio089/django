ecb7b60202284ac214f3a5aa98e1d33a5881a137
caio campos <ccamposs2007@gmail.com>
2025-07-06 07:28:06 -0300

    a

--- meu_projeto/pag1/views.py
+++ meu_projeto/pag1/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def pagina1(request):
-    return render(request, 'templates/pagina1.html')  # <-- repara nesse caminho
+    return render(request, 'pag1/pagina1.html')  # <-- repara nesse caminho
 
