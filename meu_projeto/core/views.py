d4d067dcbc85a215843ac4d93823c69b1a7c8015
caio campos <ccamposs2007@gmail.com>
2025-07-16 13:24:21 -0300

    add db cofrreto

--- meu_projeto/core/views.py
+++ meu_projeto/core/views.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request) :
     return render(request, 'index.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Substitua 'login' pelo nome da sua url de login
