91ad55c7e641dd7279d73239486a15d8617abfb7
caiocampos <ccamposs2007@gmail.com>
2025-08-14 14:20:33 -0300

    deploy 1

--- meu_projeto/meu_projeto/settings.py
+++ meu_projeto/meu_projeto/settings.py
@@ -24,17 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v^)n%-m%6%2=ysq-mm2^6u7juf+=%%oz@8s^z8!#uywd@$ezx^'
+SECRET_KEY =' django-insecure-v^)n%-m%6%2=ysq-mm2^6u7juf+=%%oz@8s^z8!#uywd@$ezx^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']  # ou ['meusite.onrender.com'] depois que tiver o link
+ALLOWED_HOSTS = ['dojo-on.onrender.com']  # ou ['meusite.onrender.com'] depois que tiver o link
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://meusite.onrender.com',  # troque pelo seu domínio
+    'https://dojo-on.onrender.com',  # troque pelo seu domínio
 ]
 
 
@@ -143,10 +143,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
