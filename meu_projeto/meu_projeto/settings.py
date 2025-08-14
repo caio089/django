54e22f9161f3265cc9bd9720dd598ad4268f44ae
caio campos <ccamposs2007@gmail.com>
2025-08-14 16:40:47 -0300

    a

--- meu_projeto/meu_projeto/settings.py
+++ meu_projeto/meu_projeto/settings.py
@@ -97,13 +97,11 @@ WSGI_APPLICATION = 'meu_projeto.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
